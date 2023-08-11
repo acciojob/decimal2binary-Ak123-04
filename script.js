1ca7b0f2c34281a03db9c9a7ed66afe1222142c2
Ak123-04 <93091049+Ak123-04@users.noreply.github.com>
2023-08-11 13:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let=rem,i=1;
+	let bin=0;
+	while(num!=0){
+		rem=num%2;
+		num=parseInt(num/2);
+		bin=bin+rem*i;
+		i=i*10;
+	}
+  cosole.log(bin);
 }
-
 window.decimalToBinary = decimalToBinary;
